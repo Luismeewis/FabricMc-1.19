f5d4b31857b6fb818c08a9695c64e66d56bda00a
Luismeewis <luisangel_rmm2113@hotmail.com>
2022-09-03 18:31:48 -0500

    Commit

---
+++ src/main/java/net/luismeewis/tutorialmod/TutorialMod.java
@@ -0,0 +1,21 @@
+package net.luismeewis.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.luismeewis.tutorialmod.block.ModBlocks;
+import net.luismeewis.tutorialmod.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+	}
+}
