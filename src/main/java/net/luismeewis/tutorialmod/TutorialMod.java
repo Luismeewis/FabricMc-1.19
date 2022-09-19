6fe3f62f6140e56b6f473354479a9583537c9e22
Luismeewis <luisangel_rmm2113@hotmail.com>
2022-09-19 18:37:25 -0500

    Custom Paintings

--- src/main/java/net/luismeewis/tutorialmod/TutorialMod.java
+++ src/main/java/net/luismeewis/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.luismeewis.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 import net.luismeewis.tutorialmod.block.ModBlocks;
 import net.luismeewis.tutorialmod.item.ModItems;
+import net.luismeewis.tutorialmod.painting.ModPaintings;
 import net.luismeewis.tutorialmod.villager.ModVillagers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +21,8 @@ public class TutorialMod implements ModInitializer {
 
 		ModVillagers.registerVillagers();
 		ModVillagers.registerTrades();
+
+		ModPaintings.registerPaintings();
 
 	}
 }
