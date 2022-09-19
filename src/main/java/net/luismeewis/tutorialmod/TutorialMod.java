883fe788d9878e0e637e203fb87a0d991a1fec5b
Luismeewis <luisangel_rmm2113@hotmail.com>
2022-09-19 00:29:49 -0500

    Custom Villagers

--- src/main/java/net/luismeewis/tutorialmod/TutorialMod.java
+++ src/main/java/net/luismeewis/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.luismeewis.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 import net.luismeewis.tutorialmod.block.ModBlocks;
 import net.luismeewis.tutorialmod.item.ModItems;
+import net.luismeewis.tutorialmod.villager.ModVillagers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +17,9 @@ public class TutorialMod implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+		ModVillagers.registerVillagers();
+		ModVillagers.registerTrades();
 
 	}
 }
